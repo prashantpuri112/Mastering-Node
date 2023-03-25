a25a6b7160dc49acec3a89a563fc5f364336672c
Prashant Puri <ppuri7069@gmail.com>
2023-03-25 10:24:42 +0545

    Reading and Writing Files

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const fs = require('fs')
 
-const hello = 'Hello world';
-console.log(hello);
+//Reading Files
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8'); // not specifing utf-8 might get back a buffer
+
+console.log(textIn);
+
+//Writing Files
+
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log('File written!');
