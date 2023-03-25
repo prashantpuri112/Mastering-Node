e5495cb90a0dd8ee918fe4a9b60c94a449a307c4
Prashant Puri <ppuri7069@gmail.com>
2023-03-25 19:25:33 +0545

    HTML Templating: Filling the Templates

--- Introduction to Node.js and NPM/html_templating.js
+++ Introduction to Node.js and NPM/html_templating.js
@@ -3,6 +3,28 @@ const http = require('http');
 const { dirname } = require('path');
 const url = require('url')
 
+//SERVER
+
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
+
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, `utf-8`);
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, `utf-8`);
+const tempProduct = fs.readFileSync(`${__dirname}/templates/templete-product.html`, `utf-8`);
+
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, `utf-8`);
 const dataObj = JSON.parse(data); // convert the data to a javascript object
 
@@ -10,16 +32,27 @@ const dataObj = JSON.parse(data); // convert the data to a javascript object
 const server = http.createServer((req, res) => {  // request, response
     const pathName = req.url;
 
+    // Overview page
     if (pathName === '/' || pathName === '/overview') {
-        res.end('This is the OVERVIEW');
+        res.writeHead(200, { 'content-type': 'text/html' });
+
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join(''); // join the array to a string
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+        res.end(output);
+
+        //Product page
     }
     else if (pathName === '/product') {
         res.end('This is the PRODUCT');
     }
+
+    //API
     else if (pathName === '/api') {
 
         res.writeHead(200, { 'content-type': 'application/json' }); // set the header to json
         res.end(data); // send the data to the client
+
+        //Not found
 
     } else {
         res.writeHead(404, {
