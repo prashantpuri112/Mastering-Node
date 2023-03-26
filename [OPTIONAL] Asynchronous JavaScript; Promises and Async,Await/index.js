eef82768ed8a46b233f2e99cc3e7e7fb5b6c2660
Prashant Puri <ppuri7069@gmail.com>
2023-03-26 23:43:54 +0545

    Building Promises

--- [OPTIONAL] Asynchronous JavaScript; Promises and Async,Await/index.js
+++ [OPTIONAL] Asynchronous JavaScript; Promises and Async,Await/index.js
@@ -1,19 +1,36 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, 'utf-8', (err, data) => { // utf-8 is the encoding type
+const readFilePro = file => { // readFilePro is a function that returns a promise
+    return new Promise((resolve, reject) => { // Promise is a constructor function
+        fs.readFile(file, (err, data) => { // fs.readFile is asynchronous
+            if (err) reject('I could not find that file 😢'); // reject is a function that is called when the promise is rejected
+            resolve(data); // resolve is a function that is called when the promise is fulfilled        
+        });
+    });
+};
+
+const writeFilePro = (file, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(file, data, err => {
+            if (err) reject('Could not write the file 😢');
+            resolve('success');
+        });
+    });
+}
+
+
+readFilePro(`${__dirname}/dog.txt`).then(data => { // readFilePro is a function that returns a promise
     console.log(`Breed: ${data}`);
 
-    superagent
-        .get(`https://dog.ceo/api/breed/${data}/images/random`) // https://dog.ceo/dog-api/
-        .then(res => { // res is the response object
-            console.log(res.body.message); // res.body is the body of the response object
-
-            fs.writeFile('dog-img.txt', res.body.message, err => { // fs.writeFile is asynchronous
-                if (err) return console.log(err.message);
-                console.log('Random dog image saved to file!');
-            });
-        }).catch(err => {
-            console.log(err.message);
-        });
-});
+    return superagent
+        .get(`https://dog.ceo/api/breed/${data}/images/random`)
+}).then(res => { // res is the response object
+    console.log(res.body.message); // res.body is the body of the response object
+    return writeFilePro('dog-img.txt', res.body.message);
+}).then(() => {
+    console.log('Random dog image saved to file!');
+})
+    .catch(err => {
+        console.log(err);
+    });
