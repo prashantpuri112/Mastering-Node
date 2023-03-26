0da24975d86eb3f869805f75445e4948bc355b2d
Prashant Puri <ppuri7069@gmail.com>
2023-03-26 23:56:20 +0545

    Consuming Promises with Async/Await

--- [OPTIONAL] Asynchronous JavaScript; Promises and Async,Await/index.js
+++ [OPTIONAL] Asynchronous JavaScript; Promises and Async,Await/index.js
@@ -17,11 +17,30 @@ const writeFilePro = (file, data) => {
             resolve('success');
         });
     });
-}
+};
 
+const getDogPic = async () => {
+    try {
+        const data = await readFilePro(`${__dirname}/dog.txt`);
+        console.log(`Breed: ${data}`);
 
-readFilePro(`${__dirname}/dog.txt`).then(data => { // readFilePro is a function that returns a promise
-    console.log(`Breed: ${data}`);
+        const res = await superagent.get(
+            `https://dog.ceo/api/breed/${data}/images/random`
+        );
+        console.log(res.body.message);
+
+        await writeFilePro('dog-img.txt', res.body.message);
+        console.log('Random dog image saved to file!');
+    } catch (err) {
+        console.log(err);
+    }
+};
+getDogPic();
+
+/*
+
+readFilePro(`${ __dirname } / dog.txt`).then(data => { // readFilePro is a function that returns a promise
+    console.log(`Breed: ${ data }`);
 
     return superagent
         .get(`https://dog.ceo/api/breed/${data}/images/random`)
@@ -34,3 +53,5 @@ readFilePro(`${__dirname}/dog.txt`).then(data => { // readFilePro is a function 
     .catch(err => {
         console.log(err);
     });
+
+*/
