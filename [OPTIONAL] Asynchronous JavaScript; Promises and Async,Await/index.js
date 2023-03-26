bdf5704349b192bc907e8f58e98a514655306a3e
Prashant Puri <ppuri7069@gmail.com>
2023-03-26 23:29:47 +0545

    From Callback Hell to Promises

--- [OPTIONAL] Asynchronous JavaScript; Promises and Async,Await/index.js
+++ [OPTIONAL] Asynchronous JavaScript; Promises and Async,Await/index.js
@@ -4,15 +4,16 @@ const superagent = require('superagent');
 fs.readFile(`${__dirname}/dog.txt`, 'utf-8', (err, data) => { // utf-8 is the encoding type
     console.log(`Breed: ${data}`);
 
-    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, res) => {
-        if (err) return console.log(err.message);
-        console.log(res.body.message);
+    superagent
+        .get(`https://dog.ceo/api/breed/${data}/images/random`) // https://dog.ceo/dog-api/
+        .then(res => { // res is the response object
+            console.log(res.body.message); // res.body is the body of the response object
 
-        fs.writeFile('dog-img.txt', res.body.message, err => {
-            console.log('Random dog image saved to file!');
+            fs.writeFile('dog-img.txt', res.body.message, err => { // fs.writeFile is asynchronous
+                if (err) return console.log(err.message);
+                console.log('Random dog image saved to file!');
+            });
+        }).catch(err => {
+            console.log(err.message);
         });
-    });
-
-
-
 });
